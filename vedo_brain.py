8e64202499334608a8828cff89c3c8a077a05058
Clara Weber <88279664+claraweber@users.noreply.github.com>
2022-03-19 18:10:01 +0100

    Update vedo_brain.py

--- vedo_brain.py
+++ vedo_brain.py
@@ -48,110 +48,6 @@ outputgifname = "output"
 
 im4=im1*3+im2
 
-#---vedo settings---
-#https://vedo.embl.es/autodocs/content/vedo/settings.html
-#xtitle = 'x'
-#ytitle = 'y'
-#ztitle = 'z'
-#defaultFont = 'Normografo' 
-#screeshotScale = 1
-#screenshotTransparentBackground = False
-#screeshotLargeImage = False 
-#computeNormals = None
-#autoResetScalarRange = True
-#allowInteraction = True
-#enableDefaultMouseCallbacks = True
-#enableDefaultKeyboardCallbacks = True
-#immediateRendering = True
-#rendererFrameColor = None
-#rendererFrameAlpha = 0.5
-#rendererFrameWidth = 0.5
-#rendererFramePadding = 0.001
-#windowSplittingPosition = None
-#enablePrintColor = True
-#usingQt = False
-#renderLinesAsTubes = False
-#pointSmoothing = False
-#lineSmoothing = False
-#polygonSmoothing = False
-#hiddenLineRemoval = False
-#visibleGridEdges = False
-#lightFollowsCamera = False
-#twoSidedLighting = True
-#useDepthPeeling = False
-#alphaBitPlanes  = True  
-#multiSamples    = 0  
-#maxNumberOfPeels= 8     
-#occlusionRatio  = 0.0
-#useFXAA = False
-#preserveDepthBuffer = False
-#useSSAO         = False
-#SSAORadius      = 0.5   # the SSAO hemisphere radius
-#SSAOBias        = 0.01  # the bias when comparing samples
-#SSAOKernelSize  = 32    
-#SSAOBlur        = False # blurring of the ambient occlusion (helps for low samples nr)
-#usePolygonOffset    = False
-#polygonOffsetFactor = 0.1
-#polygonOffsetUnits  = 0.1
-#interpolateScalarsBeforeMapping = True
-#useParallelProjection = False
-#tiffOrientationType = 4
-#annotatedCubeColor      = (0.75, 0.75, 0.75)
-#annotatedCubeTextColor  = None 
-#annotatedCubeTextScale  = 0.2
-#annotatedCubeTexts      = ["right","left ", "front","back ", " top ", "bttom"]
-#k3dMenuVisibility = True
-#k3dPlotHeight = 512
-#k3dAntialias  = True
-#k3dLighting   = 1.2
-#k3dCameraAutoFit = True
-#k3dGridAutoFit= True
-#k3dAxesHelper = True    # size of the small triad of axes on the bottom right
-#k3dPointShader= "3d"    # options are '3d', 'mesh', '3dSpecular', 'dot', 'flat'
-#k3dLineShader = "thick" # options are 'flat', 'mesh', 'thick'
-#-------------------
-
-#options for Volume
-#https://vedo.embl.es/autodocs/content/vedo/volume.html#interpolatetovolume
-#https://vedo.embl.es/autodocs/_modules/vedo/volume.html
-#c (list,str) – sets colors along the scalar range, or a matplotlib color map name
-#alphas (float,list) – sets transparencies along the scalar range
-#alphaUnit (float) – default 1, low values make composite rendering look brighter and denser
-### 10 will appear ghostish and 0.001 very dense and surface like
-#origin (list) – set volume origin coordinates
-#spacing (list) – voxel dimensions in x, y and z.
-#dims (list) – specify the dimensions of the volume.
-#mapper (str) – either ‘gpu’, ‘opengl_gpu’, ‘fixed’ or ‘smart’
-#mode (int) –define the volumetric rendering style:
-###0, composite rendering
-###1, maximum projection rendering (appears smoother)
-###2, minimum projection (not applicable)
-###3, average projection (not applicable)
-###4, additive mode (will give black cube with white brain)
-
-#color options
-#'Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu',
-#'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r',
-#'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired',
-#'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu',
-#'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r',
-#'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn',
-#'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r',
-#'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r',
-#'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r',
-#'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis',
-#'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix',
-#'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r',
-#'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r',
-#'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot', 'gnuplot2',
-#'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno',
-#'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r',
-#'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow',
-#'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10',
-#'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain',
-#'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted',
-#'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r'
-
 ###===define cameras===
 cam1=dict(pos=(180, -400, -400),
           viewup=(1,0,0),
@@ -159,8 +55,6 @@ cam1=dict(pos=(180, -400, -400),
           clippingRange=(300,400),
           focalPoint=(150,50,50)
           )
-
-#========================================
 
 
 ###===show significant changes, grey and white matter===
